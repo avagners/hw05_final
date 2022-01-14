b25866924b9af0d96fcc47fc5c466997e38bf5b4
Alexander Vagner <onemybox@gmail.com>
2022-01-14 15:50:05 +0300

    исправил название переменной

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -124,15 +124,15 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     # Подписаться на автора
-    user = get_object_or_404(User, username=username)
-    if request.user != user:
-        Follow.objects.get_or_create(user=request.user, author=user)
+    author = get_object_or_404(User, username=username)
+    if request.user != author:
+        Follow.objects.get_or_create(user=request.user, author=author)
     return redirect('posts:profile', username)
 
 
 @login_required
 def profile_unfollow(request, username):
     # Дизлайк, отписка
-    user = get_object_or_404(User, username=username)
-    Follow.objects.filter(user=request.user).filter(author=user).delete()
+    author = get_object_or_404(User, username=username)
+    Follow.objects.filter(user=request.user).filter(author=author).delete()
     return redirect('posts:profile', username)
